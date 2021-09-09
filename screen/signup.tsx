73e0ae008c6f7ef6b7e286b86188a47340087edb
SinYooDong <ud4mo@yumis.io>
2021-09-09 18:50:29 +0900

    코드완성

--- screen/signup.tsx
+++ screen/signup.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useState } from 'react'
 import { Text, View, TextInput, Button, Alert } from 'react-native'
 import database from '@react-native-firebase/database';
 import auth from '@react-native-firebase/auth';
+import analytics from '@react-native-firebase/analytics';
 
 const SignupScreen = () => {
 
@@ -14,35 +15,63 @@ const SignupScreen = () => {
             <TextInput placeholder={"이메일를 입력해주세요."} value={email} onChangeText={setEmail} />
             <TextInput secureTextEntry={true} placeholder={"비밀번호를 입력해주세요."} value={password} onChangeText={setPassword} />
             <Button title={"완료"} onPress={async (_) => {
+                await analytics().logSignUp({
+                    method: "email"
+                })
+                await analytics().logEvent("signup", {
+                    email: email,
+                })
+                Alert.alert("확인", "회원가입하시겠습니까?", [
+                    {
+                        text: "예",
+                        onPress: () => {
+                            auth()
+                                .createUserWithEmailAndPassword(email, password)
+                                .then(async (user) => {
+                                    console.log('User account created & signed in!');
+                                    const snapshot = await database().ref("/users").once('value')
 
-                auth()
-                    .createUserWithEmailAndPassword(email, password)
-                    .then(async (user) => {
-                        console.log('User account created & signed in!');
-                        const snapshot = await database().ref("/users").once('value')
+                                    database().ref("/users/" + (snapshot.numChildren()))
+                                        .set({ email: email, password: password, uid: user.user.uid })
+                                        .then(result => {
+                                            console.log(result);
+                                            Alert.alert("회원가입 완료")
+                                            analytics().logEvent("signup_success", {
+                                                email: email,
+                                                uid: user.user.uid
+                                            })
+                                        })
+                                        .catch(err => {
+                                            console.error(err);
+                                            analytics().logEvent("signup_error", {
+                                                email: email,
+                                            })
+                                            Alert.alert("에러가 발생했습니다.")
+                                        })
+                                })
+                                .catch(error => {
+                                    if (error.code === 'auth/email-already-in-use') {
+                                        console.log('That email address is already in use!');
+                                    }
 
-                        database().ref("/users/" + (snapshot.numChildren()))
-                            .set({ email: email, password: password, uid: user.user.uid })
-                            .then(result => {
-                                console.log(result);
-                                Alert.alert("회원가입 완료")
-                            })
-                            .catch(err => {
-                                console.error(err);
-                                Alert.alert("에러가 발생했습니다.")
-                            })
-                    })
-                    .catch(error => {
-                        if (error.code === 'auth/email-already-in-use') {
-                            console.log('That email address is already in use!');
+                                    if (error.code === 'auth/invalid-email') {
+                                        console.log('That email address is invalid!');
+                                    }
+
+                                    console.error(error);
+                                });
                         }
-
-                        if (error.code === 'auth/invalid-email') {
-                            console.log('That email address is invalid!');
+                    },
+                    {
+                        text: "아니요",
+                        onPress: () => {
+                            analytics().logEvent("signup_error", {
+                                email: email,
+                            })
                         }
+                    }
+                ])
 
-                        console.error(error);
-                    });
 
 
             }} />
